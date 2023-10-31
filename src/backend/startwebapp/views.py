0f9a96a2273293d6c6fa12d83cb3e21d97b327b2
Niall Kelly <niall.kelly88@mail.dcu.ie>
2023-10-31 11:12:05 +0000

    Successfully imported 'ace' build of IDE

--- src/backend/startwebapp/views.py
+++ src/backend/startwebapp/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>START WEB APP</h1>")
+def home_view(request):
+    return render(request, 'index.html')

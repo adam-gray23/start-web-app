497af5622b515091c0a6b4e160d22fc601e49abf
Adam Gray <adamgray8432@gmail.com>
2023-11-02 18:24:29 +0000

    added time checker for bat file

--- src/backend/backend/run-docker.bat
+++ src/backend/backend/run-docker.bat
@@ -1,3 +1,10 @@
+@echo off
+setlocal
+
+rem Record the start time
+set start_time=%time%
+echo Batch file started at %start_time%
+
 @REM build the docker image silently with no output
 docker build -t start-docker-image . > nul
 @REM run docker image and wait for it to finish
@@ -6,3 +13,12 @@ docker run --name start start-docker-image
 docker logs start > input/output.txt
 @REM remove docker container silently
 docker rm start > nul
+
+rem Record the end time
+set end_time=%time%
+echo Batch file completed at %end_time%
+
+rem Calculate the total running time
+powershell -command "[datetime]::Parse('%end_time%') - [datetime]::Parse('%start_time%')" > time_elapsed.txt
+
+endlocal

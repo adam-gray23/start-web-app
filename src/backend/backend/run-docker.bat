05cf45e3959584b482bc0372b2ff234f7c83e137
Niall Kelly <niall.kelly88@mail.dcu.ie>
2023-11-09 14:59:13 +0000

    fix docker container i/o

--- src/backend/backend/run-docker.bat
+++ src/backend/backend/run-docker.bat
@@ -1,4 +1,4 @@
 docker build -t start-docker-image . > nul 2>&1
-docker run --name start start-docker-image > nul 2>&1
-docker logs start > C:\Users\peter\Documents\Work\DCU\case4\project\2024-ca400-kellyn88-graya27\src\backend\backend\input\output.txt
+docker run --name start -v C:\Users\02nke\dcu\yr4\4yp\2024-ca400-kellyn88-graya27\src\backend\backend\input\input.txt:/app/input.txt start-docker-image /app/input.txt > nul 2>&1
+docker logs start > C:\Users\02nke\dcu\yr4\4yp\2024-ca400-kellyn88-graya27\src\backend\backend\input\output.txt
 docker rm start > nul 2>&1
